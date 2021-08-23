2d15cbb55e5d740f351551d3751c0f1acd19ed9a
conmcdon721 <81780150+conmcdon721@users.noreply.github.com>
2021-08-22 22:34:06 -0400

    First commit

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+import tmi from 'tmi.js'
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const BOT_USERNAME = process.env.BOT_USERNAME
+export const OAUTH_TOKEN = 'oauth:' + process.env.OAUTH_TOKEN
+export const CHANNEL_NAME = '#evanwahr'
+
+export const messages = [
+    'streamer Evan Wahr has a small dong! Hes rocking two inches, on a good day!!',
+    'streamer Evan Wahr is HUNG! Im talking big MASSIVE cock!',
+    'streamer Evan Wahr hasnt had genitals since the accident! Quit asking!'
+]
