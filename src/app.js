dcc02766a69b16dda56bd46291e3ba2db543fcf6
conmcdon721 <81780150+conmcdon721@users.noreply.github.com>
2021-10-25 20:02:39 -0400

    Fix authentication issues with new token

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import tmi from 'tmi.js'
 import { BOT_USERNAME, OAUTH_TOKEN, CHANNEL_NAME, guestStreamer, messages } from './constants.js';
 
+console.log(process.env)
+
 const options = {
 	options: { debug: true, messagesLogLevel: "info" },
 	connection: {
